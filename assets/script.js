32602a921df3a3f27d7429520922b514e7d90bd9
Marko <marko@milicevic.fr>
2023-09-28 14:18:07 +0200

    Step 4: Switch slides over dots and arrows

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,7 @@ const slides = [
 /**
  * @private
  *
- * Bind the left and right arrows
+ * Bind the left and right arrows to navigate trough slides
  *
  * @param {Object} state - The slider state
  */
@@ -33,14 +33,36 @@ function bindArrows(state) {
 	leftArrowElement.addEventListener("click", (event) => {
 		// Click on the left arrow
 		event.preventDefault(); // Prevent default `<a href="#">` behaviour
-		console.log("Click on the left arrow");
+		goToPreviousSlide(state);
 	});
 
 	rightArrowElement.addEventListener("click", (event) => {
 		// Click on the right arrow
 		event.preventDefault(); // Prevent default `<a href="#">` behaviour
-		console.log("Click on the right arrow");
+		goToNextSlide(state);
 	});
+}
+
+/**
+ * @private
+ *
+ * Go to the previous slide
+ *
+ * @param {Object} state - The slider state
+ */
+function goToPreviousSlide(state) {
+	switchSlide(state, state.currentIndex - 1);
+}
+
+/**
+ * @private
+ *
+ * Go to the next slide
+ *
+ * @param {Object} state - The slider state
+ */
+function goToNextSlide(state) {
+	switchSlide(state, state.currentIndex + 1);
 }
 
 /**
@@ -65,7 +87,7 @@ function generateDots(state) {
 /**
  * @private
  *
- * Bind all dots
+ * Bind all dots to navigate trough slides
  *
  * @param {Object} state - The slider state
  */
@@ -77,8 +99,63 @@ function bindDots(state) {
 
 		dotElement.addEventListener("click", (event) => {
 			event.preventDefault(); // Prevent default `<a href="#">` behaviour
-			console.log(`Need to switch to the #${index} slide`);
+			switchSlide(state, index);
 		});
+	}
+}
+
+/**
+ * @private
+ *
+ * Switch to the new slide (also update dots to reflect the new current slide)
+ *
+ * @param {Object} state - The slider state
+ * @param {Number} newIndex - The slider new index (0 = first slide)
+ * @throws {Error} The new index is out of slides index
+ */
+function switchSlide(state, newIndex) {
+	if (newIndex < 0 || newIndex >= state.slides.length) {
+		throw new Error("Out of slides index");
+	}
+
+	// Update the state
+	state.currentIndex = newIndex;
+	state.currentSlide = state.slides[newIndex];
+
+	// Replace the image
+	const bannerImgElement = state.rootElement.querySelector(".banner-img");
+	bannerImgElement.src = `${state.imagesBaseUrl}${state.currentSlide.image}`;
+
+	// Replace the paragraph
+	const paragraphElement = state.rootElement.querySelector("p");
+	paragraphElement.innerHTML = state.currentSlide.tagLine;
+
+	// Update dots
+	updateDots(state);
+}
+
+/**
+ * @private
+ *
+ * Update all dots by selecting the current slide's dot and unselecting all other dots
+ *
+ * @param {Object} state - The slider state
+ */
+function updateDots(state) {
+	const dotElements = state.rootElement.querySelectorAll(".dot");
+
+	for (let index = 0; index < dotElements.length; index++) {
+		const dotElement = dotElements[index];
+
+		if (index === state.currentIndex) {
+			// This slide is currently displayed
+			dotElement.removeAttribute("href"); // Restore the default cursor over the link
+			dotElement.classList.add("dot_selected"); // It's omit if the class is already present
+		} else {
+			// This slide is the previous / next
+			dotElement.href = "#"; // Apply the right cursor over the link
+			dotElement.classList.remove("dot_selected"); // It's omit if the class is not yet present
+		}
 	}
 }
 
@@ -88,17 +165,25 @@ function bindDots(state) {
  * @param {Object} config - The slider configuration
  * @param {HTMLElement} config.rootElement - The root element of the slider (in case when several sliders required on the same page)
  * @param {Array} config.slides - The list of all available slides
+ * @param {Number} config.startAtIndex - The slider starting index (0 = first slide)
+ * @param {String} config.imagesBaseUrl - The base URL for loading slides images (ex: CDN base URL, relative path, ...)
  * @throws {Error} Something goes wrong in the slider
  */
-function runSlider({ rootElement, slides }) {
-	if (!rootElement || !slides || !slides.length) {
+function runSlider({ rootElement, slides, startAtIndex, imagesBaseUrl }) {
+	if (!rootElement || !slides || !slides.length || !imagesBaseUrl) {
 		throw new Error("Missing arguments for running the slider");
+	}
+	if (startAtIndex < 0 || startAtIndex > slides.length - 1) {
+		throw new Error("Start index is out of slides");
 	}
 
 	// This object represents the state of the slider component
 	const state = {
 		rootElement,
 		slides: [...slides], // Duplicate the reference in order to not update slides after slider started
+		imagesBaseUrl,
+		currentIndex: startAtIndex,
+		currentSlide: slides[startAtIndex],
 	};
 
 	// Arrows HTML are already present in the page, not needed to generate dynamically
@@ -106,15 +191,18 @@ function runSlider({ rootElement, slides }) {
 
 	generateDots(state);
 	bindDots(state);
-};
 
-// End slider component
+	// Current slide HTML already present in the page, not needed to generate dynamically
+	switchSlide(state, startAtIndex);
+}
 
 // Run the slider
 try {
 	runSlider({
 		rootElement: document.getElementById("banner"),
 		slides,
+		startAtIndex: 0,
+		imagesBaseUrl: "./assets/images/slideshow/",
 	});
 } catch (err) {
 	console.error(`An error occurred with the slider: ${err.message}`);
